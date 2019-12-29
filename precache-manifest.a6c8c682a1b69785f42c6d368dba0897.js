13085160cfb23f57b5498e4264253dcbbb32b39b
divyeshrana9775 <divyeshrana9775@gmail.com>
2019-12-28 21:56:50 -0500

    Updates

---
+++ precache-manifest.a6c8c682a1b69785f42c6d368dba0897.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d661254c22c7e9d55d8b335f4e6b57d",
+    "url": "/cardlist/index.html"
+  },
+  {
+    "revision": "3ddb226f0e7cc532b0a2",
+    "url": "/cardlist/static/css/main.7ccf09cf.chunk.css"
+  },
+  {
+    "revision": "6e5fd916aef5fe8ffc7b",
+    "url": "/cardlist/static/js/2.7fbac707.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cardlist/static/js/2.7fbac707.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ddb226f0e7cc532b0a2",
+    "url": "/cardlist/static/js/main.ac75dead.chunk.js"
+  },
+  {
+    "revision": "c6dc013ef6451bf5b3eb",
+    "url": "/cardlist/static/js/runtime-main.48ebda17.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cardlist/static/media/logo.5d5d9eef.svg"
+  }
+]);
